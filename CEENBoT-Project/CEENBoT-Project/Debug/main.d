830ed6e9ceeb9330ea2986cef5ca109a92f8ae99
AbdoKRS <89417636+AbdoKRS@users.noreply.github.com>
2024-03-13 14:10:40 -0500

    AA

---
+++ CEENBoT-Project/CEENBoT-Project/Debug/main.d
@@ -0,0 +1,139 @@
+main.d main.o: .././main.c \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/capi324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/utils324v221.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/sys324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/psxc324v221.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom324p.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/spi324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/tmrsrvc324v221.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/isr324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/lcd324v221.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/tiny324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/spkr324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/led324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/mega324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/step324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/swatch324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/usonic324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/uart324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/adc324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/spiflash324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/ti324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/i2c324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/cmucam324v221.h \
+ c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/pixy324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/bbtk324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/cbot324v221.h \
+ ../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/capi324v221.h
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/capi324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/utils324v221.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/sys324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/psxc324v221.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom324p.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/spi324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/tmrsrvc324v221.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/isr324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/lcd324v221.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/tiny324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/spkr324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/led324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/mega324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/step324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/swatch324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/usonic324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/uart324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/adc324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/spiflash324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/ti324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/i2c324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/cmucam324v221.h:
+
+c:\users\akrs7\onedrive\documents\ecen\ 345\ceenbot\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/pixy324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/bbtk324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/cbot324v221.h:
+
+../../../../../ECEN\ 345/CeenBot/capi324v22x-v2.06.000R/lib-includes/capi324v221.h:
